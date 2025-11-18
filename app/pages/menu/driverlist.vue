b4d0f7d56b60037a51b703e25e4c1c78c5254a4f
David-Hrb <hrbacekd08@gmail.com>
2025-11-18 16:31:52 +0100

    17

--- app/pages/menu/driverlist.vue
+++ app/pages/menu/driverlist.vue
@@ -1,77 +1,31 @@
 <template>
-  <div class="maincont">
-    <div class="container-calendar">
-      <h1 class="title">Všichni jezdci</h1>
-      
-      <div class="table-wrapper">
-        <table class="calendar-table">
-          <thead>
-            <tr>
-              <th>Avatar</th>
-              <th>Číslo</th>
-              <th>Národnost</th>
-              <th>Jméno</th>
-              <th>Narození</th>
-              <th>Tým</th>
-              <th>Popularita</th>
-              <th>Ego</th>
-              <th>Kontrakt</th>
-              <th>Placený</th>
-              <th>Koncentrace</th>
-              <th>Předjíždění</th>
-              <th>Zkušenosti</th>
-              <th>Rychlost</th>
-              <th>Výdrž</th>
-              <th>Jednej</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr 
+  <div class="drivercont">
+    <div class="driverDiv currentDriver">
+      <h2>Aktuální jezdec</h2>
+    </div>
+    <div class="driverDiv ListOfDrivers">
+      <h2>Všichni jezdci</h2>
+      <div class="allDriversList">
+            <div 
               v-for="(driver, index) in drivers"
               :key="index"
-              class="table-row"
             >
-              <td><img :src="`/images/avatars/${giveavatar(driver.avatar)}.svg`" class="avatar" alt="avatar" style="width: 40px;"></td>
-              <td class="race-number">{{ index + 1 }}</td>            
-              <td><span class="fi" :class="`fi-${driver.nationality}`" aria-hidden="true"></span> {{ driver.nationality }}</td>
-              <td class="circuit-name">{{ driver.name }}</td>
-              <td>{{ driver.bornyear }}</td>          
-              <td>{{ isNaN(driver.currentteam) ? "bez týmu" : driver.currentteam }}</td>     
-              <td> {{ driver.popularity }} </td>
-              <td> {{ driver.ego }} </td>
-              <td> {{ driver.contractexp }}</td>
-              <td> {{ driver.payeddriver === false ? "NE" : "ANO" }}</td>
-              <td> {{ driver.concentration }} </td>
-              <td> {{ driver.overtaking }} </td>
-              <td> {{ driver.experience }} </td>
-              <td> {{ driver.quickness }} </td>
-              <td> {{ driver.stamina }} </td>
-              <td> 
-                <button>Podepiš</button>
-              </td>
-            </tr>
-          </tbody>
-           <thead>
-            <tr>
-              <th>Avatar</th>
-              <th>Číslo</th>
-              <th>Národnost</th>
-              <th>Jméno</th>
-              <th>Narození</th>
-              <th>Tým</th>
-              <th>Popularita</th>
-              <th>Ego</th>
-              <th>Kontrakt</th>
-              <th>Placený</th>
-              <th>Koncentrace</th>
-              <th>Předjíždění</th>
-              <th>Zkušenosti</th>
-              <th>Rychlost</th>
-              <th>Výdrž</th>
-              <th>Jednej</th>
-            </tr>
-          </thead>
-        </table>
+              <img :src="`/images/avatars/${giveavatar(driver.avatar)}.svg`" class="avatar" alt="avatar" style="width: 40px;">
+              {{ index + 1 }}        
+              <span class="fi" :class="`fi-${driver.nationality}`" aria-hidden="true"></span> {{ driver.nationality }}
+              {{ driver.name }}
+              {{ driver.bornyear }}         
+              {{ isNaN(driver.currentteam) ? "bez týmu" : driver.currentteam }}   
+              {{ driver.popularity }} 
+              {{ driver.ego }} 
+              {{ driver.contractexp }}
+              {{ driver.payeddriver === false ? "NE" : "ANO" }}
+              {{ driver.concentration }} 
+              {{ driver.overtaking }}
+              {{ driver.experience }} 
+              {{ driver.quickness }} 
+              {{ driver.stamina }} 
+          </div>
       </div>
     </div>
    </div>
@@ -84,6 +38,8 @@ drivers.value = await $fetch("/api/listDriver");
 function giveavatar(num) {
   return avatars[num.toString()] 
 }
+
+
 definePageMeta({ layout: 'menu' }) 
 
 const switchLayout = inject('switchLayout')
