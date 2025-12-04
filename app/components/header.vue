200153d18bb6216007e0ee388e068bff55b0cd54
David-Hrb <hrbacekd08@gmail.com>
2025-12-04 08:54:20 +0100

    50

--- app/components/header.vue
+++ app/components/header.vue
@@ -14,7 +14,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="60%" viewBox="0 0 24 24">
           <path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z"/>
         </svg>
-        <span>YOUR TEAM</span>
+        <span>Váš tým</span>
       </NuxtLink>
     </div>
     
